189ab1980903aa5e2d61513403520d5435a3bc3e
sasikumarr123 <sasikumar.rajeswaran@gmail.com>
2018-08-15 19:22:10 +0530

    model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserlistComponent } from './userlist/userlist.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserlistComponent,
+    UserdetailsComponent
   ],
   imports: [
     BrowserModule
